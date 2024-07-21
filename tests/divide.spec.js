f5326a7faa3404df7c43775ec1aa6dc122e6e444
Betaf <beta.falc2@gmail.com>
2024-07-21 12:44:46 +0200

    done with antonia

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -12,3 +12,31 @@ describe("Iteration 2", () => {
 })
 
 */
+
+describe("Iteration 1 | Getting Started", () => {
+    describe("Function divide", () => {
+      it("should be defined", () => {
+        expect(divide).toBeDefined();
+      });
+  
+      it("should take two arguments", () => {
+        expect(divide.length).toBe(2);
+      });
+  
+      it("should return the division of the two numbers", () => {
+        expect(divide(1, 2)).toEqual(0,5);
+        expect(divide(3, 4)).toEqual(0,75);
+        expect(divide(100, 50)).toEqual(2);
+      });
+  
+      it("should return undefined if any of the arguments is not provided", () => {
+        expect(divide(1)).toEqual(undefined);
+        expect(divide()).toEqual(undefined);
+        expect(divide(undefined, 1)).toEqual(undefined);
+      });
+
+      it("should return undefined if any of the arguments is not a valid number", () => {
+        expect(divide("")).toEqual(undefined);
+      });
+    });
+  });
